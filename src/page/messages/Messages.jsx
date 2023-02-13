4501beaf49905c326afae04f6098233d6ff340aa
dahalapaar <contact.apaardahal@gmail.com>
2023-02-13 11:04:34 +0545

    Setup completed

---
+++ src/page/messages/Messages.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './messages.scss'
+
+const Messages = () => {
+  return (
+    <div className='messages'>Messages</div>
+  )
+}
+
+export default Messages
---
+++ src/page/myGigs/Mygigs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './mygigs.scss'
+
+const Mygigs = () => {
+  return (
+    <div className='mygigs'>Mygigs</div>
+  )
+}
+
+export default Mygigs
