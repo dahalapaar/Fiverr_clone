65ffda6cd12df1ceff1f0c023856fd112cc5396d
dahalapaar <contact.apaardahal@gmail.com>
2023-02-15 16:00:00 +0545

    Featured Section Completed

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './featured.scss'
+
+const Featured = () => {
+  return (
+    <div className='featured'>
+        <div className="container">
+            <div className="left">
+                <h1>Find the perfect <i>freelance</i> services for your business</h1>
+                <div className="search">
+                    <div className="searchInputs">
+                        <img src='./images/search.png' alt=''/>
+                        <input type="text" placeholder='Try Building Mobile App'/>
+                    </div>
+                    <button>Search</button>
+                </div>
+                <div className="popular">
+                    <span>Popular:</span>
+                    <button>Web Design</button>
+                    <button>Wordpress</button>
+                    <button>Logo Design</button>
+                    <button>Mobile Application</button>
+                    <button>AI Search</button>
+                </div>
+            </div>
+            <div className="right">
+                <img src='./images/apaar_thumbnail.png' alt=''/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Featured from '../../components/featured/Featured'
 import './home.scss'
 
 const Home = () => {
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <Featured/>
+    </div>
   )
 }
 
