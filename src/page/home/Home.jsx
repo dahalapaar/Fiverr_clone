8f067d32f01d59e1a57b0b94a5dadb83c98dc65d
dahalapaar <contact.apaardahal@gmail.com>
2023-02-16 11:18:36 +0545

    Slider Component Completed

--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import Slide from '../../components/slide/Slide'
 import Trustedby from '../../components/trustedBy/Trustedby'
+import CatCard from '../../components/catCard/CatCard'
+import {cards} from '../../data';
 import './home.scss'
 
 const Home = () => {
@@ -8,6 +11,11 @@ const Home = () => {
     <div className='home'>
       <Featured/>
       <Trustedby/>
+      <Slide slidesToShow={5} arrowsScroll={4}>
+        {cards.map(card => (
+            <CatCard item={card} key={card.id}/>
+            ))}
+      </Slide>
     </div>
   )
 }
