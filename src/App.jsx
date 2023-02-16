f734bef925061b82e6cc962170db228587383862
dahalapaar <contact.apaardahal@gmail.com>
2023-02-16 10:17:45 +0545

    Trusted By Section Completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import Footer from "./components/footer/Footer";
 import Home from "./page/home/Home";
 import Gigs from "./page/gigs/Gigs";
 import Gig from "./page/gig/Gig";
-import Login from "./page/login/Login";
-import Register from "./page/register/Register";
+// import Login from "./page/login/Login";
+// import Register from "./page/register/Register";
 import Add from "./page/add/Add";
-import Orders from "./page/orders/Orders";
+// import Orders from "./page/orders/Orders";
 import Messages from "./page/messages/Messages";
 import Message from "./page/message/Message";
 import MyGigs from "./page/myGigs/Mygigs";
@@ -42,10 +42,10 @@ function App() {
           path: "/myGigs",
           element: <MyGigs />,
         },
-        {
-          path: "/orders",
-          element: <Orders />,
-        },
+        // {
+        //   path: "/orders",
+        //   element: <Orders />,
+        // },
         {
           path: "/messages",
           element: <Messages />,
@@ -64,14 +64,14 @@ function App() {
         },
       ],
     },
-    {
-      path: "/register",
-      element: <Register />,
-    },
-    {
-      path: "/login",
-      element: <Login />,
-    },
+    // {
+    //   path: "/register",
+    //   element: <Register />,
+    // },
+    // {
+    //   path: "/login",
+    //   element: <Login />,
+    // },
   ]);
 
   return <RouterProvider router={router} />;
--- src/page/home/Home.jsx
+++ src/page/home/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Featured from '../../components/featured/Featured'
+import Trustedby from '../../components/trustedBy/Trustedby'
 import './home.scss'
 
 const Home = () => {
   return (
     <div className='home'>
       <Featured/>
+      <Trustedby/>
     </div>
   )
 }
